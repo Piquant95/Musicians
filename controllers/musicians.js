09ef659ae9810789d073b7ca15d0c9e106f4d8c4
piquant95 <piquant95@yahoo.com>
2023-03-06 13:03:02 -0500

    index.ejs setup

--- controllers/musicians.js
+++ controllers/musicians.js
@@ -5,7 +5,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('', (req, res) => {
-    res.send('<h1>This is my list of musicians</h1>')
+    res.render('musicians/index.ejs')
 })
 
 
