602d44aae2c3262cd4fbbf9e6b181dac805439f9
piquant95 <piquant95@yahoo.com>
2023-03-07 11:44:49 -0500

    Models setup

---
+++ models/Users.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+// const Schema = new mongoose.Schema
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+        type: String,
+        required: [true, "Please provide a name"],
+        unique: [true, "Must be original."]
+       }, 
+        password: {
+        type: String,
+        required: [true, "Please provide the URL for an image"],
+       }
+   },
+   {
+       timestamps: true
+   }
+
+);
+
+
+const Users = mongoose.model('Users', userSchema)
+
+module.export = Users;
